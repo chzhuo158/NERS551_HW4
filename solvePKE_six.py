1f21f92c76ac58bf5e15454490d6a131ef53f4ba
Zhuo Chen <chzhuo@umich.edu>
2022-03-14 11:06:13 -0400

    forgot to push the six-group solver

--- solvePKE_six.py
+++ solvePKE_six.py
@@ -2,6 +2,7 @@
 
 # This code correspond to the Part C-2 of HW4
 
+from ctypes.wintypes import DOUBLE
 import numpy as np
 
 # init parameters
@@ -15,9 +16,9 @@ LAMBD_prev = 2.6E-15
 LAMBD0 = 2.6E-15
 
 # one-group beta and lambd
-# beta_eff = 0.0076
+beta_eff = 0.0076
 # beta_eff_prev = 0.0076
-# lambd = 0.49405
+lambd = 0.49405
 # lambd_prev = 0.49405
 
 # six-group beta and lambd
@@ -25,6 +26,13 @@ beta  = [0.02584,0.152,0.13908, 0.30704, 0.1102, 0.02584]
 beta_eff_prev = beta
 lambd = [0.0128, 0.0318, 0.119, 0.3181, 1.4027, 3.9286]
 lambd_prev = lambd 
+
+lambd_tilda = np.empty(6, dtype=DOUBLE) 
+OMEGA   = np.empty(6, dtype=DOUBLE) 
+G_prev  = np.empty(6, dtype=DOUBLE) 
+zeta_hat= np.empty(6, dtype=DOUBLE) 
+zeta    = np.ones(6, dtype=DOUBLE) 
+zeta_prev = np.ones(6, dtype=DOUBLE) 
 
 alpha = 0
 lambd_H = 0
@@ -40,8 +48,6 @@ P0 = 1.0
 p = P0*ffp
 p_prev = P0*ffp_prev
 p_pprev = P0*ffp_prev
-zeta = 1.0
-zeta_prev = 1.0
 theta = 0.5
 delta_p_left = 0.0
 delta_p_right = 0.0
@@ -57,7 +63,7 @@ def k0(x):
     return 1.0-x/2.0+x*x/6.0-x*x*x/24.0+x*x*x*x/120.0-x*x*x*x*x/720.0
 
 # output_file = "PKE_sol" + "_dt_" +str("{:.1e}".format(dt)) + "_Lambda_" +str("{:.1e}".format(LAMBD)) + ".out" 
-output_file = "PKE_sol.out"
+output_file = "PKE_sol_mg.out"
 fout = open(output_file,'w')
 
 #w = 1.0
@@ -65,24 +71,26 @@ for i in range(int(n)):
     # step 0 -- set the reactivity curve
     t = i/1000
     if i<=1000:
-        rho_im = 0.5*beta_eff*t
+        rho_im = 0.5*t
     else:
         # rho_im = 0.5*beta_eff-0.5*beta_eff*(i-1000)/5000
-        rho_im = 0.5*beta_eff-0.1*beta_eff*(t-1)
+        rho_im = 0.5-0.1*(t-1)
 
     # step 1 -- Determine transformation parameter with eq. (23)
     #        -- Prepare xx and xx with eq. (15),(16) and (17),
     #           the exponential integration functions (10)~(13) are called
     alpha = 1.0/dt_prev*np.log(p_prev/p_pprev)
-    lambd_tilda = (lambd+alpha)*dt
-    OMEGA = LAMBD0/LAMBD*beta_eff*dt*k1(lambd_tilda)
-    G_prev = LAMBD0/LAMBD_prev*beta_eff_prev*p_prev
-    zeta_hat = np.exp(-lambd*dt)*zeta_prev + np.exp(alpha*dt)*dt*G_prev*(k0(lambd_tilda)-k1(lambd_tilda))
+    for k in range(6):
+        lambd_tilda[k] = (lambd[k]+alpha)*dt
+        OMEGA[k] = LAMBD0/LAMBD*beta[k]*dt*k1(lambd_tilda[k])
+        G_prev[k] = LAMBD0/LAMBD_prev*beta_eff_prev[k]*p_prev
+        zeta_hat[k] = np.exp(-lambd[k]*dt)*zeta_prev[k] \
+                    + np.exp(alpha*dt)*dt*G_prev[k]*(k0(lambd_tilda[k])-k1(lambd_tilda[k]))
 
     # step 2 -- Prepare xx, xx and xx with eq. (18),(19)
-    tau = lambd*OMEGA
-    Sd_hat = lambd*zeta_hat
-    Sd_prev = lambd_prev*zeta_prev
+    tau     = np.dot(lambd,OMEGA)           #lambd*OMEGA
+    Sd_hat  = np.dot(lambd,zeta_hat)        #lambd*zeta_hat
+    Sd_prev = np.dot(lambd_prev,zeta_prev)  #lambd_prev*zeta_prev
 
     # step 3 -- Prepare a1,b1 with eq. (32) and (33) 
     lambd_H_tilda = (lambd_H+alpha)*dt
@@ -94,7 +102,8 @@ for i in range(int(n)):
     # step 4 -- Prepare a,b,c with eq. (36), (37), and(38)
     a = theta*dt*a1/LAMBD
     b = theta*dt*(((b1-beta_eff)/LAMBD-alpha)+tau/LAMBD0)-1
-    c = theta*dt/LAMBD0*Sd_hat+np.exp(alpha*dt)*((1-theta)*dt*((((rho_prev-beta_eff)/LAMBD_prev-alpha)*p_prev+Sd_prev/LAMBD0)+p_prev))
+    c = theta*dt/LAMBD0*Sd_hat\
+        +np.exp(alpha*dt)*((1-theta)*dt*((((rho_prev-beta_eff)/LAMBD_prev-alpha)*p_prev+Sd_prev/LAMBD0)+p_prev))
 
     # step 5 -- Evaluate the flux level with eq. (39) or (40)
     if a<0:
@@ -135,6 +144,6 @@ for i in range(int(n)):
     zeta = p*OMEGA + zeta_hat
     zetan.append(zeta)
 
-    fout.write("%12f  %12f  %12f %12f \n" % (rho, p, zeta, i/1000) )
+    fout.write("%12f  %12f  %12f %12f \n" % (np.sum(rho), p, np.sum(zeta), i/1000) )
 
 fout.close()
